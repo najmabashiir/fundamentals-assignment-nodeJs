8457bfc2b6cfc89ab4338b26079c2be3bebbb948
najmabashiir <najaxbashiirali>
2026-02-10 19:08:29 +0300

    adding name using array
    Signed-off-by: najmabashiir <najaxbashiirali>

---
+++ app.js
@@ -0,0 +1,14 @@
+let Myarry=[{
+    name:"Eng Najma Bashiir Ali",
+
+}]
+function addobject(){
+    Myarry.push({
+        name:"Eng Ayuub Abdalla"
+    })
+    console.log(Myarry);
+
+}
+addobject()
+
+
